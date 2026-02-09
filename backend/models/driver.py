94bea7597404653ee31635e67f0779d9344a6116
Ying Xi <amberng999@gmail.com>
2026-02-09 11:55:04 +0800

    Add current location to driver

--- backend/models/driver.py
+++ backend/models/driver.py
@@ -5,6 +5,7 @@ from typing import Tuple
 class Driver:
     id: int
     start_location: Tuple[float, float]  #(x, y)
+    current_location: Tuple[float, float]  #(x, y)
     vehicle_type: str                    # 'fuel' or 'ev'
     capacity: float = 0.0                # in kg, default 0.0
     available: bool = True
@@ -14,6 +15,8 @@ class Driver:
         return Driver(
             id=d["id"],
             start_location=tuple(d["start_location"]),
+            current_location=tuple(d["current_location"])
+            if "current_location" in d else tuple(d["start_location"]),
             vehicle_type=d["vehicle_type"],
             capacity=float(d.get("capacity", 0.0)),
             available=d.get("available", True)
